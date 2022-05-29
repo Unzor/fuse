d1c601bc590069b4ca82cd41364244a3e25108a8
Unzor <71789103+Unzor@users.noreply.github.com>
2022-05-28 21:02:35 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,40 +1,63 @@
+#!/usr/bin/env node
+
 const express = require('express');
 const fs = require("fs");
-
+var parser = require("cli2json");
+var args = parser.parse(process.argv.slice(2).join(" "), {
+    readCommandAfter: ["-u", "--use", "-a", "--attach"]
+})
 const app = express();
 app.use(express.static(__dirname));
 
-function FUSE_FN(fn){
-	var fne;
-	app.get("/fuse/run/" + fn.name, async (req, res) => {
-		if (req.query.args) {
-			var args = atob(req.query.args).split(",");
-      if (fn.constructor.name == "AsyncFunction") {
-			  res.send(await fn(...args))
-      } else {
-        res.send(fn(...args))
-      }
-		} else {
-			if (fn.constructor.name == "AsyncFunction") {
-			  res.send(await fn(...args))
-      } else {
-        res.send(fn(...args))
-      }
-		}
-	})
-  }
-
-if (fs.existsSync("preload.json")) {
-  var preload = JSON.parse(fs.readFileSync("preload.json").toString());
-  preload.files.forEach(function(file){
-    eval(fs.readFileSync(file).toString())
-  })
+function arrayFindIncludes(r, n) {
+    var u;
+    var a = null;
+    n.forEach(function(n) {
+        if (n.includes(r)) {
+            u = n;
+            a = n
+        } else {
+            u = a
+        }
+    });
+    return u
 }
+
+function FUSE_FN(fn) {
+    var fne;
+    app.get("/fuse/run/" + fn.name, async (req, res) => {
+        if (req.query.args) {
+            var args = atob(req.query.args).split(",");
+            if (fn.constructor.name == "AsyncFunction") {
+                res.send(await fn(...args))
+            } else {
+                res.send(fn(...args))
+            }
+        } else {
+            if (fn.constructor.name == "AsyncFunction") {
+                res.send(await fn(...args))
+            } else {
+                res.send(fn(...args))
+            }
+        }
+    })
+}
+
+if (arrayFindIncludes("-u", args.flags) || arrayFindIncludes("--use", args.flags)) {
+    var preload = JSON.parse(fs.readFileSync(args.flags[0].split(" ")[1]).toString());
+    preload.files.forEach(function(file) {
+        eval(fs.readFileSync(file).toString())
+    })
+} else if (arrayFindIncludes("-a", args.flags) || arrayFindIncludes("--attach", args.flags)) {
+    console.log(args.flags[0].split(" ")[1])
+    eval(fs.readFileSync(args.flags[0].split(" ")[1]).toString())
+}
+
 app.get('/fuse/attach/:code', (req, res) => {
-  eval(atob(req.params.code))
-  res.send("attached")
+    eval(atob(req.params.code))
+    res.send("attached")
 });
 
 app.listen(3000, () => {
-  console.log('server started');
-});
+    console.log('Fuse server started')
+})
